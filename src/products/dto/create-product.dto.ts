5acd35042cf7f75ade6616c241458080472e4cd9
Matias-V823 <matias.varas62@gmail.com>
2026-03-31 14:30:49 -0300

    feat: enhance product creation with category validation and update DTO

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateProductDto {
+    @IsNotEmpty({ message: 'El nombre del producto es obligatorio' })
+    @IsString({message: 'Nombre no válido'})
+    name: string
+    @IsNotEmpty({ message: 'El precio es obligatorio' })
+    @IsNumber({maxDecimalPlaces:2}, {message:'Precio no válido'})
+    price: number
+    @IsString({ message: 'descripción no valida' })
+    description: string
+    @IsNotEmpty({ message: 'El valor del inventario es obligatorio' })
+    @IsNumber({ maxDecimalPlaces: 0 }, { message: 'Cantidad no valida' })
+    inventory: number
+    @IsNotEmpty({ message: 'La categoría es obligatoria' })
+    @IsInt({ message: 'Categoría no valida' })
+    categoryId: number
+}
