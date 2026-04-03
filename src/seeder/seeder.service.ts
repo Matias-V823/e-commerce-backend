36e63261dea123dcbfdc13fb25d7762bba0317e1
Matias-V823 <matias.varas62@gmail.com>
2026-04-03 15:35:52 -0300

    feat: add categories and products seed data with seeder service integration

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from '../categories/entities/category.entity';
+import { Repository } from 'typeorm';
+import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
 export class SeederService {
+    constructor(
+        @InjectRepository(Category) private readonly categoryRepository : Repository<Category>,
+        @InjectRepository(Product) private readonly productRepository : Repository<Product>
+    ){}
     async seed() {
         console.log('Desde seed en seeder.service')
     }
